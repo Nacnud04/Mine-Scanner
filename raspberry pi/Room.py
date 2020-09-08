faa4bff0348a470935926a0ca8c7d8b10f2ba791
Nacnud04 <61326047+Nacnud04@users.noreply.github.com>
2020-09-08 17:29:18 -0400

    Update Room.py

--- raspberry pi/Room.py
+++ raspberry pi/Room.py
@@ -1,127 +1,121 @@
-#Stations
-class Stations:
+#Rooms
+class Room:
     
-    num_of_stations = 1
-    unwanted = 'abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ:'
+    num_of_rooms = 0
     
-    def __init__(self, name, number):
+    def __init__(self, name, number, intStation):
+        Room.num_of_rooms += 1
         self.name = name
         self.number = number
-        Stations.num_of_stations += 1
+        self.intStation = intStation
         
-    def generateData(self):
-        import time
-        from gpiozero import LED
-        import serial
-        from statistics import median
+    def room2DScan(self, points):
+        import sys
+        import os
+        from math import cos, sin, pi, floor
+        from adafruit_rplidar import RPLidar
         try :
-            ser = serial.Serial('/dev/ttyACM0',9600)
-            print("Establishing Serial on /dev/ttyACM0...")
+            PORT_NAME = '/dev/ttyUSB0'
+            lidar = RPLidar(None, PORT_NAME)
         except :
-            try :
-                ser = serial.Serial('/dev/ttyACM1',9600)
-                print("Establishing Serial on /dev/ttyACM1...")
-            except :
-                print("No microcontroller found")
-        shot = LED(26)
-        distance = []
-        x, y, z = [], [], []
-        xnew, ynew, znew= "", "", ""
-        shot.on()
-        time.sleep(.1)
-        shot.off()
-        data = ser.readline()
-        print(data)
-        time.sleep(5)
-        shot.on()
-        time.sleep(.1)
-        shot.off()
-        for i in range(2):
-            data = ser.readline()
-            print(data)
-            if data == b'Position Data:\r\n':
-                for i in range(401):
-                    data = ser.readline()
-                    data = data.decode("utf-8")
-                    print(data)
-                    xnew, ynew, znew = data.split(",")
-                    for letter in Stations.unwanted :
-                        xnew = xnew.replace(letter,'')
-                    for letter in Stations.unwanted :
-                        ynew = ynew.replace(letter,'')
-                    for letter in Stations.unwanted :
-                        znew = znew.replace(letter,'')
-                    xnew = round(float(xnew), 3)
-                    ynew = round(float(ynew), 3)
-                    znew = round(float(znew), 3)
-                    print(str(xnew)+","+str(ynew)+","+str(znew))
-                    x.append(float(xnew))
-                    y.append(float(ynew)) 
-                    z.append(float(znew))
-            elif data == b'Distance Data:\r\n':
-                for i in range(31):
-                    data = ser.readline()
-                    data = float(data.decode("utf-8"))
-                    print(data)
-                    distance.append(data)
-                print('Shot Complete')
-        self.pitch = median(x)
-        self.roll = median(y)
-        self.yaw = median(z)
-        self.dist = median(distance)
-        print('Done')
-
-    def degToPosition(self, prevxpos, prevypos, prevzpos, key):
-        import matplotlib.pyplot as plt
-        import matplotlib.patches as patches
-        import math
-        distance = self.dist
-        pitch = math.radians(self.pitch)
-        roll = math.radians(self.roll)
-        yaw = math.radians(self.yaw)
-        self.x = distance + float(prevxpos)
-        self.y = float(prevypos)
-        self.z = float(prevzpos)
-        if key == '-3D':
-            #Add X Rotations Influence
-            self.y = self.y*math.cos(pitch)-self.z*math.sin(pitch)
-            self.z = self.y*math.sin(pitch)+self.z*math.cos(pitch)
-            #Add Y Rotations Influence
-            self.x = self.x*math.cos(roll)+self.z*math.sin(roll)
-            self.z = -self.x*math.sin(roll)+self.z*math.cos(roll)
-        if key == '-2D' or key == '-3D':
-            #Add Z Rotations Influence :
-            self.x = self.x*math.cos(yaw)-self.y*math.sin(yaw)
-            self.y = self.x*math.sin(yaw)+self.y*math.cos(yaw)
-        else :
-            print("Key unknown")
+            PORT_NAME = '/dev/ttyUSB1'
+            lidar = RPLidar(None, PORT_NAME)
+        max_distance = 0
+        x = []
+        y = []
+        scan_data = [0]*360     
+        try :
+            print(lidar.info)
+            for scan in lidar.iter_scans():
+                for (_, angle, distance) in scan:
+                    scan_data[min([359, floor(angle)])] = distance
+                for angle in range(360):
+                    distance = scan_data[angle]
+                    if distance > 0:
+                        max_distance = max([min([5000, distance]), max_distance])
+                        radians = angle * pi / 180.0
+                        if len(x) >= int(points):
+                            distance = 'this is supposed to cause an error'
+                        xnew = distance * cos(radians)
+                        ynew = distance * sin(radians)
+                        print('x:' + str(xnew) + ' y:' + str(ynew))
+                        xnew = round(xnew, 3)/25.4
+                        ynew = round(ynew, 3)/25.4
+                        x.append(xnew)
+                        y.append(ynew)
+        except TypeError:
+            print('Stoping')
+        print(len(x))
+        lidar.stop()
+        lidar.disconnect()
+        self.x2d = x
+        self.y2d = y
+    
+    def addOffset2D(self, start, end, statoffx, statoffy):
+        x = self.x2d
+        y = self.y2d
+        for i in range(360):
+            x[i] += statoffx
+            y[i] += statoffy
+        self.x2d = x
+        self.y2d = y
         
-    def show(self, x, y, z):
+    def rotate2Ddata(self, yaw):
+        import math
+        for i in range(len(self.x2d)):
+            self.x2d[i] = self.x2d[i]*math.cos(yaw)-self.y2d[i]*math.sin(yaw)
+            self.y2d[i] = self.x2d[i]*math.sin(yaw)+self.y2d[i]*math.cos(yaw)
+        
+    def showRoom(self, data):
         import matplotlib.pyplot as plt
-        import matplotlib.patches as patches
-        from PIL import Image
-        ax = plt.axes(projection='3d')
-        ax.scatter(x[0:self.number + 1], y[0:self.number + 1], z[0:self.number + 1], marker = 's', color = 'grey')
-        ax.scatter(x[0:self.number + 1], y[0:self.number + 1], z[0:self.number + 1], marker = 'x', color = 'black')
-        ax.plot3D(x[0:self.number + 1], y[0:self.number + 1], z[0:self.number + 1], 'gray')
-        ax.scatter(x[0], y[0], z[0], marker = 's', color = 'red')
-        ax.scatter(x[self.number], y[self.number], z[self.number], marker = 's', color = 'green')
-        ax.set_ylabel('y')
-        ax.set_zlabel('z')
-        ax.set_xlabel('x')
-        print(x)
-        print(y)
-        print(z)
-        #For equal scaling create bounding box
-        import numpy as np
-        max_range = np.array([max(x)-min(y), max(y)-min(y), max(z)-min(z)]).max()
-        Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(max(x)+min(x))
-        Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(max(y)+min(y))
-        Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(max(z)+min(z))
-        #Plot bounding box
-        for xb, yb, zb in zip(Xb, Yb, Zb):
-           ax.plot([xb], [yb], [zb], 'w')
-        #Save plot
-        plt.savefig('statmap.png', bbox_inches='tight')
-        image = Image.open('statmap.png')
-        image.save('statmap.ppm')
+        import math
+        plt.style.use('seaborn-whitegrid')
+        plt.plot(self.x2d, self.y2d, 'o', color='black')
+        plt.axes().set_aspect('equal')
+        plt.show()
+        x = self.x2d
+        y = self.y2d
+        xline = []
+        yline = []
+        points = len(x)
+        pointx = x[0]
+        pointy = y[0]
+        x.pop(0)
+        y.pop(0)
+#          for i in range(points):
+#             mindist = 1000000000000000000000000
+#             for j in range(len(x)):
+#                 try:
+#                     print(j)
+#                     print(len(x))
+#                     print(len(xline))
+#                     dist = math.sqrt((x[j+1]-pointx)**2+(y[j+1]-pointy)**2)
+#                     if dist <= mindist:
+#                         mindist = dist
+#                         pointx = x[j+1]
+#                         pointy = y[j+1]
+#                         pnum = j
+#                 except IndexError:
+#                     xline.append(x[pnum])
+#                     yline.append(y[pnum]) 
+#                     x.pop(pnum)
+#                     y.pop(pnum)
+        import matplotlib.pyplot as pltlin
+        allx = []
+        ally = []
+        for i in range(len(data)):
+            allx.extend(data[i].x2d)
+            ally.extend(data[i].y2d)
+            print(data[i].x2d)
+            print(data[i].y2d)
+        allx.extend(self.x2d)
+        ally.extend(self.y2d)
+        fig, (ax1, ax2) = pltlin.subplots(nrows=1, ncols=2)
+        print(allx)
+        print(ally)
+        ax1.scatter(allx, ally, color='black')
+        ax2.plot(self.x2d, self.y2d)
+#         pltlin.axes().set_aspect('equal')
+        ax1.axis('square')
+        ax2.axis('square')
+        pltlin.show()
