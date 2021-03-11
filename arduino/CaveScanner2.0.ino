2e780747791b46d247c2187feb0983af6c82ba35
Nacnud04 <61326047+Nacnud04@users.noreply.github.com>
2021-03-11 08:34:30 -0500

    Update CaveScanner2.0.ino

--- arduino/CaveScanner2.0.ino
+++ arduino/CaveScanner2.0.ino
@@ -11,6 +11,39 @@ int i = 0;
 int callibration=8,SHOT=9;
 int azimuth;
 
+//New (better) control program for MPU6050
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  #include "Wire.h"
+#endif
+MPU6050 mpu;
+#define OUTPUT_READABLE_QUATERNION
+#define INTERRUPT_PIN 2
+#define LED_PIN 13
+bool blinkState = false;
+// control variables
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+// motion and orientation variables
+Quaternion q;
+VectorInt16 aa;
+VectorInt16 aaReal;
+VectorInt16 aaWorld;
+VectorFloat gravity;
+float euler[3];
+float ypr[3];
+
+// interrupt detection
+volatile bool mpuInterrupt = false;
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
 void setup(){
   pinMode(SHOT, INPUT);
   pinMode(callibration, INPUT);
@@ -21,28 +54,7 @@ void setup(){
   Wire.endTransmission(true);
   Serial.begin(9600);
   pinMode(2, INPUT);
-  while (digitalRead(callibration) == LOW){
-  }
-  Serial.println("Callibrating");
-  while (i <= 1000) {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B);  
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU,12,true);  
-    AcX=Wire.read()<<8|Wire.read();    
-    AcY=Wire.read()<<8|Wire.read();  
-    AcZ=Wire.read()<<8|Wire.read();  
-    GyX=Wire.read()<<8|Wire.read();  
-    GyY=Wire.read()<<8|Wire.read();  
-    GyZ=Wire.read()<<8|Wire.read(); 
-    AcXCal += AcX;
-    AcYCal += AcY;
-    AcZCal += AcZ;
-    delay(10);
-    i += 1;
-  } 
-  AcXCal = AcXCal / 1000; AcYCal = AcYCal / 1000; AcZCal = AcZCal / 1000;
-  qmc.init();
+  i2csetup();
   Serial.println("Done Callibrating");
 }
 
@@ -54,6 +66,67 @@ void loop(){
     shot();
   }
   delay(20);
+}
+
+void i2csetup(){
+  // join i2c bus
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000);
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400,true);
+  #endif
+  
+  // initialize device(s)
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+  // verify connection(s)
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  
+  // start mpu when ready
+  Serial.println(F("\nWaiting for callibration pin to go high: "));
+  while (digitalRead(callibration) == LOW){
+  }
+  // load & config DMP
+  Serial.println(F("Initalizing DMP..."));
+  devStatus = mpu.dmpInitialize();
+  // gyro offsets:
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788);
+  
+  // continue if it worked
+  if (devStatus == 0) {
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+    
+    // enable interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    // state ready
+    Serial.println(F("DMP ready, waiting for first interrupt..."));
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  
+  else {
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void shot(){
